0cb2a8c6fce4d7eca4398c444b77196c4b10f588
Mike <n@n.n>
2019-08-11 00:30:27 +0300

    initial upload

--- neural.py
+++ neural.py
@@ -24,13 +24,12 @@ class SymmetricalNeuralNetwork:
         """weights must be numpy matrix (dtype=float), which sets the weights of the neural netwrok
         dimensionality determines the number of neurons in each layer (including constant 1 in input layer, but not including constant 1 in output)
         so far 3 input neurons and 2 output neurons the shape must be 2x4"""
-
+        assert symmetry_mat.shape==weights.shape, "symmetry and weights must match in shape"
         self._symmetry_mat = symmetry_mat
         self.weights = weights
 
     def compute(self, input):
-        assert input.shape == tuple((self.weights.shape[1] - 1,)), f"wrong input dimension, required dimensions are {
-        self.weights.shape[1] - 1}"
+        assert input.shape == tuple((self.weights.shape[1] - 1,)), f"wrong input dimension, required dimensions are {self.weights.shape[1] - 1}"
 
         extended_input = np.insert(input, 0, 1, 0)  # insert constant 1 in zero place
 
@@ -39,7 +38,7 @@ class SymmetricalNeuralNetwork:
         return NextLayerInput
 
     def apply_symetry(self, weights_mat):
-        for i in range(self._symmetry_mat.max() + 1):
+        for i in np.unique(self._symmetry_mat[self._symmetry_mat>0]):
             original = weights_mat[self._symmetry_mat == i]
             if not original.size:
                 continue
@@ -48,8 +47,12 @@ class SymmetricalNeuralNetwork:
             weights_mat[self._symmetry_mat == i] = original_value
         return weights_mat
 
-    def mutable_weights(self):
-        return np.logical_or(self._symmetry_mat % 1 != 0, self._symmetry_mat > 0)
+    def _mutable_weights(self):
+        self.mutables=np.zeros(self._symmetry_mat.shape,dtype=np.bool)
+        for i in np.unique(self._symmetry_mat[self._symmetry_mat > 0]):
+            first=np.where(self._symmetry_mat==i)[0]
+            self.mutables[first]=True
+        return self.mutables
 
     @property
     def weights(self):
@@ -60,3 +63,4 @@ class SymmetricalNeuralNetwork:
         assert len(new_weights.shape) == 2
         assert new_weights.shape == self._symmetry_mat.shape
         self._weights = self.apply_symetry(new_weights).copy()
+        self._mutable_weights()
